4f855b4dde4e21eaecb0e75817c1ed412c22b2f4
Phnc <paulohn27@hotmail.com>
2020-07-29 21:06:58 -0300

    Adding Slick Slider

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import PageDefault from '../../../components/PageDefault'
+import PageDefault from '../../../components/PageDefault';
 
 function CadastroCategoria() {
-    return(
+  return (
+    <PageDefault>
+      <h1>Cadastro de Categoria</h1>
 
-        <PageDefault>
+      <form>
 
-            <h1>Cadastro de Categoria</h1>
+        <label>
+          Nome da Categoria:
+          <input
+            type="text"
+          />
+        </label>
 
-            <Link to="/">
+        <button>
+          Cadastrar
+        </button>
+      </form>
 
-                Ir para home
 
-            </Link>
-
-        </PageDefault>
-
-    );
+      <Link to="/">
+        Ir para home
+      </Link>
+    </PageDefault>
+  )
 }
 
 export default CadastroCategoria;
